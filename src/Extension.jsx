98b6f33eb567f5985ae4860ea509ce4be37e09b4
sdob <sdob@cs.ucc.ie>
2018-01-13 07:34:14 +0000

    Pass in elements from main

--- src/Extension.jsx
+++ src/Extension.jsx
@@ -2,26 +2,12 @@
 import React, { Fragment } from 'react';
 import * as items from './items';
 
-export default function Extension() {
+export default function Extension({ elements }) {
   return (
     <Fragment>
       <h3>Library</h3>
       <ul className="you_icon cf">
-        {
-          Object.keys(items)
-            .filter(key => key !== 'USEFUL_ITEMS')
-            .filter(key => !!document.querySelector(`div#infoBarQImage${items[key]}`))
-            .map((key) => {
-              const li = document.querySelector(`div#infoBarQImage${items[key]}`)
-                .parentElement
-                .parentElement;
-              // Useful items should be duplicated rather than moved
-              if (!items.USEFUL_ITEMS.includes(key)) {
-                li.style.display = 'none';
-              }
-              return <li key={key} dangerouslySetInnerHTML={{ __html: li.innerHTML }} />;
-            })
-        }
+        {elements}
       </ul>
     </Fragment>
   );
